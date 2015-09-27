9e9cf4fe6df21552ead362c3c18c5e6086bf87cb
tincan24 <tincan24@gmail.com>
2015-09-27 16:01:49 -0700

    Add canMoveTo & makeMoveTo methods

--- src/main/java/com/jasonderek/tetris/Board.java
+++ src/main/java/com/jasonderek/tetris/Board.java
@@ -9,12 +9,14 @@ import javax.swing.*;
 
 public class Board extends JPanel implements ActionListener {
 
-    private static final int DELAY = 500;
+    private static final int DELAY = 100;
 
     private Timer timer;
     private JLabel statusBar;
     private int boardWidth;
     private int boardHeight;
+    private int curX;
+    private int curY;
 
     public Board(JLabel statusBar, int boardWidth, int boardHeight) {
         this.statusBar = statusBar;
@@ -25,10 +27,14 @@ public class Board extends JPanel implements ActionListener {
 
     public void start() {
         timer.start();
+        newPiece();
     }
 
     // fires every tick
     public void actionPerformed(ActionEvent actionEvent) {
+        if (canMoveTo(curX, curY + 1)) {
+            makeMoveTo(curX, curY + 1);
+        }
         repaint();
     }
 
@@ -37,19 +43,45 @@ public class Board extends JPanel implements ActionListener {
 
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
-                drawSquare(g, j * squareWidth(), i * squareHeight());
+                drawSquare(g, j * squareWidth(), i * squareHeight(), false);
             }
         }
+
+        drawSquare(g, curX * squareWidth(), curY * squareHeight(), true);
     }
 
-    private void drawSquare(Graphics g, int x, int y) {
-        Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102),
+    private void newPiece() {
+        curX = boardWidth / 2;
+        curY = 0;
+    }
+
+    private boolean canMoveTo(int newX, int newY) {
+        if (0 <= newX && newX < boardWidth && 0 <= newY && newY < boardHeight) {
+            return true;
+        }
+        return false;
+    }
+
+    private void makeMoveTo(int newX, int newY) {
+        curX = newX;
+        curY = newY;
+        repaint();
+    }
+
+    private void drawSquare(Graphics g, int x, int y, boolean isCurPiece) {
+        Color colors[] = { new Color(255, 255, 255), new Color(204, 102, 102),
                 new Color(102, 204, 102), new Color(102, 102, 204),
                 new Color(204, 204, 102), new Color(204, 102, 204),
                 new Color(102, 204, 204), new Color(218, 170, 0)
         };
 
-        Color color = colors[0];
+        Color color;
+        if (isCurPiece) {
+            color = colors[1];
+        }
+        else {
+            color = colors[0];
+        }
 
         g.setColor(color);
         g.fillRect(x + 1, y + 1, squareWidth() - 1, squareHeight() - 1);
