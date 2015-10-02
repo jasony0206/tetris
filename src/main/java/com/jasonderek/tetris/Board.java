ad968fe4299ae05b0f3d1cfe1e7967f975d302c2
jasony0206 <jasony0206@gmail.com>
2015-10-01 18:15:21 -0700

    WIP: Working on key listner

--- src/main/java/com/jasonderek/tetris/Board.java
+++ src/main/java/com/jasonderek/tetris/Board.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 public class Board extends JPanel implements ActionListener {
@@ -26,6 +28,8 @@ public class Board extends JPanel implements ActionListener {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         tiles = new boolean[boardWidth][boardHeight];
+
+        addKeyListener(new TetrisKeyAdapter());
     }
 
     public void start() {
@@ -38,42 +42,21 @@ public class Board extends JPanel implements ActionListener {
     }
 
     public void actionPerformedMethod() {
-        if (canMoveTo(curX, curY + 1)) {
-            makeMoveTo(curX, curY + 1);
-        }
-        else {
+        if (!tryMoveTo(curX, curY + 1)) {
             addToTiles(curX, curY);
         }
+
         repaint();
     }
 
-    public void addToTiles(int x, int y) {
-        if (0 <= x && x < boardWidth && 0 <= y && y < boardHeight) {
-            tiles[x][y] = true;
-
-            newPiece();
+    public boolean tryMoveTo(int newX, int newY) {
+        if (canMoveTo(newX, newY)) {
+            makeMoveTo(newX, newY);
+            return true;
         }
-    }
-
-    public boolean getTile(int x, int y) {
-        return tiles[x][y];
-    }
-
-    public void paint(Graphics g) {
-        System.out.println("running paint");
-
-        for (int i = 0; i < boardHeight; i++) {
-            for (int j = 0; j < boardWidth; j++) {
-                drawSquare(g, j, i, false);
-            }
+        else {
+            return false;
         }
-
-        drawSquare(g, curX, curY, true);
-    }
-
-    private void newPiece() {
-        curX = boardWidth / 2;
-        curY = 0;
     }
 
     public boolean canMoveTo(int newX, int newY) {
@@ -90,6 +73,33 @@ public class Board extends JPanel implements ActionListener {
         curX = newX;
         curY = newY;
         repaint();
+    }
+
+    public void addToTiles(int x, int y) {
+        if (0 <= x && x < boardWidth && 0 <= y && y < boardHeight) {
+            tiles[x][y] = true;
+
+            newPiece();
+        }
+    }
+
+    public boolean getTile(int x, int y) {
+        return tiles[x][y];
+    }
+
+    public void paint(Graphics g) {
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j < boardWidth; j++) {
+                drawSquare(g, j, i, false);
+            }
+        }
+
+        drawSquare(g, curX, curY, true);
+    }
+
+    private void newPiece() {
+        curX = boardWidth / 2;
+        curY = 0;
     }
 
     private void drawSquare(Graphics g, int x, int y, boolean isCurPiece) {
@@ -128,5 +138,31 @@ public class Board extends JPanel implements ActionListener {
 
     public int getBoardHeight() {
         return boardHeight;
+    }
+
+    class TetrisKeyAdapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            System.out.println("in keyPressed");
+
+            int keycode = e.getKeyCode();
+
+            switch (keycode) {
+                case KeyEvent.VK_LEFT:
+                    tryMoveTo(curX - 1, curY);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    tryMoveTo(curX + 1, curY);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    tryMoveTo(curX, curY);
+                    break;
+                case KeyEvent.VK_UP:
+                    tryMoveTo(curX, curY);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
